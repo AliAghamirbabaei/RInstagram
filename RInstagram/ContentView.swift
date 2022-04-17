72cf2f00e1384a62133be3a26477fead35f3a13f
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-18 01:37:28 +0430

    Initial Commit

---
+++ RInstagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RInstagram
+//
+//  Created by Ali Aghamirbabaei on 4/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RInstagram/RInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  RInstagramApp.swift
+//  RInstagram
+//
+//  Created by Ali Aghamirbabaei on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct RInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
