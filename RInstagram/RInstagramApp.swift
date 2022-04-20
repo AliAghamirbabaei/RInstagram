7e6805da6f1f58f6f55a96fa6095de43a351bae6
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-21 03:45:13 +0430

    Added SwiftuiPager for move between views

--- RInstagram/RInstagramApp.swift
+++ RInstagram/RInstagramApp.swift
@@ -6,18 +6,32 @@
 //
 
 import SwiftUI
+import SwiftUIPager
 
 @main
 struct RInstagramApp: App {
+    @StateObject var page: Page = .first()
+    var views = Array(0...1)
     
     init() {
-        UINavigationBar.appearance().titleTextAttributes = [.font : UIFont(name: "Quicksand-Bold", size: 24)!]
+        UINavigationBar.appearance().titleTextAttributes = [.font: UIFont(name: "Quicksand-Bold", size: 24)!]
     }
     
     var body: some Scene {
         WindowGroup {
-            HomeView()
-                .environmentObject(HomeViewModel())
+            GeometryReader { proxy in
+                let size = proxy.size
+                
+                Pager(page: self.page, data: self.views, id: \.self) {_ in
+                    DirectView()
+                        .environmentObject(DirectViewModel())
+                    
+                    HomeView()
+                        .environmentObject(HomeViewModel())
+                }
+                .frame(width: size.width, height: size.height)
+            }
+            .ignoresSafeArea()
         }
     }
 }
