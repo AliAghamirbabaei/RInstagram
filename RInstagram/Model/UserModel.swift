049a84de4a9209783a0afaf1cd5fdffcb729cea3
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-21 02:50:44 +0430

    Added family name for some users

--- RInstagram/Model/UserModel.swift
+++ RInstagram/Model/UserModel.swift
@@ -15,13 +15,13 @@ struct User {
 }
 
 // MARK: - Mocked Data
-let mockedUsers: [User] = [User(id: 0, username: "Lucien", profilePicture: "Lucien", location: "Neka', Iran"),
-                     User(id: 1, username: "Oliver", profilePicture: "Oliver", location: "Tehran, Iran"),
-                     User(id: 2, username: "Jonny", profilePicture: "Jonny", location: "Babol, Iran"),
-                     User(id: 3, username: "Mac", profilePicture: "Mac", location: "Esfahan, Iran"),
-                     User(id: 4, username: "Franklin", profilePicture: "Franklin", location: "Masjed Soleyman, Iran"),
-                     User(id: 5, username: "Eddie", profilePicture: "Eddie", location: "Rasht, Iran"),
-                     User(id: 6, username: "Neil", profilePicture: "Neil", location: "Mashhad, Iran"),
+let mockedUsers: [User] = [User(id: 0, username: "Lucien Evans", profilePicture: "Lucien", location: "Neka', Iran"),
+                     User(id: 1, username: "Oliver Smith", profilePicture: "Oliver", location: "Tehran, Iran"),
+                     User(id: 2, username: "Jonny Martinez", profilePicture: "Jonny", location: "Babol, Iran"),
+                     User(id: 3, username: "Mac Slown", profilePicture: "Mac", location: "Esfahan, Iran"),
+                     User(id: 4, username: "Franklin Fricilon", profilePicture: "Franklin", location: "Masjed Soleyman, Iran"),
+                     User(id: 5, username: "Eddie Jameson", profilePicture: "Eddie", location: "Rasht, Iran"),
+                     User(id: 6, username: "Neil May", profilePicture: "Neil", location: "Mashhad, Iran"),
                      User(id: 7, username: "Zein", profilePicture: "Zein", location: "Tabriz, Iran"),
                      User(id: 8, username: "Daniel", profilePicture: "Daniel", location: "Yazd, Iran"),
                      User(id: 9, username: "Norman", profilePicture: "Norman", location: "Shiraz, Iran")]
